6353b8becb072a638ff94972ffacd175f8a1e7df
Natália Rolim <natalia.rolim@celfocus.com>
2020-11-22 16:46:24 +0000

    create and get transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,38 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const {income, outcome} = this.transactions.reduce((total: Balance, transaction: Transaction) => {
+      switch (transaction.type) {
+        case 'income':
+          total.income += transaction.value;
+          break;
+        case 'outcome':
+          total.outcome += transaction.value;
+        default:
+          break;
+
+      }
+      return total;
+
+    }, {
+      income: 0,
+      outcome: 0,
+      total: 0
+    });
+
+    const total = income - outcome;
+    return {income, outcome, total};
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
